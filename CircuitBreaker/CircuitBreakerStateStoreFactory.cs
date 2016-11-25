5efd138e526636e3be1f7c895768b7ddc2cda484
kylos101 <kylos101@gmail.com>
2016-11-25 17:26:10 -0500

    Use Type as a key...instead of a string.

--- CircuitBreaker/CircuitBreakerStateStoreFactory.cs
+++ CircuitBreaker/CircuitBreakerStateStoreFactory.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 
 namespace CircuitBreaker
 {
     public class CircuitBreakerStateStoreFactory
     {
-        private static ConcurrentDictionary<string, ICircuitBreakerStateStore> _stateStores =
-            new ConcurrentDictionary<string, ICircuitBreakerStateStore>();
+        private static ConcurrentDictionary<Type, ICircuitBreakerStateStore> _stateStores =
+            new ConcurrentDictionary<Type, ICircuitBreakerStateStore>();
 
         internal static ICircuitBreakerStateStore GetCircuitBreakerStateStore(ICircuit circuit)
         {
@@ -13,12 +14,12 @@ namespace CircuitBreaker
             // The ConcurrentDictionary keeps track of ICircuitBreakerStateStore objects (across threads)
             // For example, a store for a db connection, web service client, and NAS storage could exist            
 
-            if (!_stateStores.ContainsKey(circuit.GetType().Name))
+            if (!_stateStores.ContainsKey(circuit.GetType()))
             {
-                _stateStores.TryAdd(circuit.GetType().Name, new CircuitBreakerStateStore(circuit));
+                _stateStores.TryAdd(circuit.GetType(), new CircuitBreakerStateStore(circuit));
             }
 
-            return _stateStores[circuit.GetType().Name];
+            return _stateStores[circuit.GetType()];
         }
 
         // TODO: Add the ability for Circuit breaker stateStores to update the state in this dictionary?        
